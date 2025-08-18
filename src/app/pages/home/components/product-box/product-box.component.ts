0413ddab7e3acca74d7549a228f0864bb4de3892
Emmy Nguyen <145530039+tastytough@users.noreply.github.com>
2025-08-18 12:31:47 -0700

    Add cart logic in cart service

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { Product } from "src/app/models/product.model";
 
 @Component({
   selector: "app-product-box",
@@ -6,7 +7,20 @@ import { Component, Input, OnInit } from "@angular/core";
 })
 export class ProductBoxComponent implements OnInit {
   @Input() fullWidthMode = false;
+  product: Product | undefined = {
+    id: 1,
+    title: "Sneaker",
+    price: 250,
+    category: "shoes",
+    description: "the best sneaker",
+    image: "https://placehold.co/150",
+  };
+  @Output() addToCart = new EventEmitter();
   constructor() {}
 
   ngOnInit(): void {}
+
+  onAddToCart(): void {
+    this.addToCart.emit(this.product);
+  }
 }
