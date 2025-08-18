2208af808eafdcdd0020f2cf07cd46a2b3fb99de
Emmy Nguyen <145530039+tastytough@users.noreply.github.com>
2025-08-18 14:54:40 -0700

    Remove item in a row

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -53,4 +53,7 @@ export class CartComponent implements OnInit {
   onClearCart(): void {
     this.cartService.clearCart();
   }
+  onRemoveFromCart(item: CartItem): void {
+    this.cartService.removeFromCart(item);
+  }
 }
