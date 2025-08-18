2f6719452da9886f10f6af6b80e1488fba95b684
Emmy Nguyen <145530039+tastytough@users.noreply.github.com>
2025-08-17 17:32:26 -0700

    Add Cart page

---
+++ src/app/pages/cart/cart.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from "@angular/core";
+import { Cart, CartItem } from "src/app/models/cart.model";
+
+@Component({
+  selector: "app-cart",
+  templateUrl: "./cart.component.html",
+})
+export class CartComponent implements OnInit {
+  cart: Cart = {
+    items: [
+      {
+        product:
+          "https://plus.unsplash.com/premium_photo-1753820185677-ab78a372b033?q=80&w=732&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        name: "Sneaker",
+        price: 150,
+        quantity: 1,
+        id: 1,
+      },
+      {
+        product:
+          "https://plus.unsplash.com/premium_photo-1753820185677-ab78a372b033?q=80&w=732&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        name: "Running Shoes",
+        price: 150,
+        quantity: 3,
+        id: 2,
+      },
+    ],
+  };
+  dataSource: Array<CartItem> = [];
+  displayedColumns: Array<string> = [
+    "product",
+    "name",
+    "price",
+    "quantity",
+    "total",
+    "action",
+  ];
+  constructor() {}
+
+  ngOnInit(): void {
+    this.dataSource = this.cart.items;
+  }
+
+  getTotal(items: Array<CartItem>): number {
+    return items
+      .map((item) => item.price * item.quantity)
+      .reduce((prev, current) => prev + current, 0);
+  }
+}
