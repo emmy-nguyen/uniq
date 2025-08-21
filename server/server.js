5e7dbf1cb824d1f4338c7d286a0d1bbb050506b9
Emmy Nguyen <145530039+tastytough@users.noreply.github.com>
2025-08-20 23:05:08 -0700

    Implement Stripe payment

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const stripe = require("stripe")(
+  "sk_test_51RyMsBKC5TTV9AjpszUoSsOPLMBIW2XReHmj7pRamlM7M3GGnzpMOo4aRLfC1rXORZlJS7TP7ub10tlRyMeqrDwc000YhUqsLG"
+);
+const express = require("express");
+const cors = require("cors");
+const bodyparser = require("body-parser");
+const app = express();
+app.use(express.static("public"));
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+app.use(cors({ origin: true, credentials: true }));
+
+const YOUR_DOMAIN = "http://localhost:4242";
+
+app.post("/checkout", async (req, res, next) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      line_items: req.body.items.map((item) => ({
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: item.name,
+            images: [item.product],
+          },
+          unit_amount: item.price * 100,
+        },
+        quantity: item.quantity,
+      })),
+      mode: "payment",
+      success_url: `${YOUR_DOMAIN}/success.html`,
+      cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+    });
+    res.status(200).json(session);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.listen(4242, () => console.log("Running on port 4242"));
